09537584b14a6d253967d4c4d008a579c9b522fe
mohamud-eslan <mohmud141@gmail.com>
2022-11-29 14:50:08 +0300

    an other commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import {useState} from 'react';
 //import Form from './component/Form';
 import './App.css';
 
-
 function App() {
   const [count, setCount] = useState(4);
-  function decrementCount(){
-    setCount(prevCount => prevCount - 1)
+   const decrementCount = ()=>{
+    setCount(count - 1)
   }
-  function incrementCount(){
-    setCount(prevCount => prevCount + 1)
+   const incrementCount = ()=>{
+    setCount(count + 1)
   }
-
-  
   return (
     <div className="App">
+      <h1>welcome to my counter Ap</h1>
+      <h2>the count is:{count}</h2>
       <button onClick={decrementCount}>-</button> 
        <span>{count}</span>
         <button onClick={incrementCount}>+</button>
--- src/component/form.js
+++ src/component/form.js
@@ -1,4 +1,19 @@
-import React from "react";
+import {useState} from "react";
+//import Form from "./component/Form"
+const Form = () =>{
+    const [inputData, setInputData] = useState({});
+    console.log(inputData);
+
+    return (
+        <div className="myDiv">
+            <input inputData ={inputData} name="name" setInputData={setInputData} ></input>
+            <input inputData ={inputData} name="location" setInputData={setInputData} ></input>
+            <input inputData ={inputData} name="photo" setInputData={setInputData} ></input>
+            <button onClick={() =>setInputData({}) }>submit</button>
+        </div>
+    )
+
+}
 
 
 export default Form;
